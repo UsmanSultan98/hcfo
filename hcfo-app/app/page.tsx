ba06c823650f042ab0bc92bfaec173b9b0e82064
Usman Sultan <usmansultan6744@gmail.com>
2024-06-27 14:28:30 +0430

    page.tsx update + global css

--- hcfo-app/app/page.tsx
+++ hcfo-app/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <h3>By Usman Sultan</h3>{" "}
-            <Image id="img-radius"
+            <Image 
               src="/prof.jpg"
               alt="Vercel Logo"
               className="dark:invert"
@@ -45,7 +45,7 @@ export default function Home() {
         <a
           href="#"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
+          target="#"
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
@@ -61,7 +61,7 @@ export default function Home() {
         <a
           href="#"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
+          target="#"
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
